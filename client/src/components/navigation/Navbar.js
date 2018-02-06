070a254a6b6a8697b258bd36d474f3f988f334a7
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2018-02-05 17:37:12 -0700

    Fixed issue with navbar

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,23 +1,27 @@
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
+import {Navbar, Nav, NavItem} from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap"
 import React from "react";
 
 
 export default function Navigation(props) {
     return (
-      <Navbar className="total-nav" inverse collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to ="/"><NavItem eventKey={1} href="#">Home</NavItem></LinkContainer>
-            <LinkContainer to ="/blog"><NavItem eventKey={2} href="#">Blog</NavItem></LinkContainer>
-          </Nav>
-         </Navbar.Collapse>
-      </Navbar>
-  )
+        <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#brand">Rate Your Wrath</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Login
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Sign up
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    )
 }
